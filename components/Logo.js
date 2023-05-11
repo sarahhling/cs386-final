31baf674ca3afd058357fa7e97a0a2a513f638c1
Sarah Ling <52732273+sarahhling@users.noreply.github.com>
2023-05-11 06:28:38 -0700

    fix img url

--- components/Logo.js
+++ components/Logo.js
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 
 function Logo() {
-  return <Image src="/../public/logo.png" width={50} height={150} alt="logo" />;
+  return <Image src="/logo.png" width={50} height={150} alt="logo" />;
 }
 
 export default Logo;
